98d03403a7b9bfad59471017ea4aa4cf5a2e44dd
kento-oka <kento-oka@kentoka.com>
2018-09-13 12:39:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fratily Skeleton Bundle
---
+++ src/SkeletonBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FratilyPHP Kernel
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento-oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Bundle\Skeleton;
+
+use Fratily\Kernel\Bundle\Bundle;
+
+class SkeletonBundle extends Bundle{
+
+}
